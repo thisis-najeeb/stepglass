eeaca6cba502be2fdf3ed35cb7b93dedddc11dd0
NAJEEB UR REHMAN <najeebrehman353@gmail.com>
2026-08-17 22:47:37 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -4,3 +4,4 @@ export { createTraceHandler } from "./langchain.js";
 export type { LangChainCompatibleHandler } from "./langchain.js";
 export type { TraceEvent, TraceEventType, RunSummary, TokenUsage } from "./types.js";
 export { estimateCost, buildUsage } from "./pricing.js";
+export { hashInput, stableStringify } from "./hash.js";
